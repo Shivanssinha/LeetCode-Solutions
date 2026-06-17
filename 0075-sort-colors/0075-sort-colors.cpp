b205690b6aaf5969d8348830ee64647d60c93f15
Shivans Sinha <7105sks@gmail.com>
2026-06-17 20:31:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& v) {
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+        for(int j = 0;j<v.size();j++){
+            if(v[j] == 0)c0++;
+            if(v[j] == 1)c1++;
+            if(v[j] == 2)c2++;
+        }
+        for(int j = 0;j<v.size();j++){
+            if(j<c0)v[j] = 0;
+            else if((j - c0)<c1)v[j] = 1;
+            else v[j] = 2;
+        }
+    }
+};
