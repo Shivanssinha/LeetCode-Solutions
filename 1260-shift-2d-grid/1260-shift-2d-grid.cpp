77c5f14942c9c2380c2cbaefeb96ac6282f298a3
Shivans Sinha <7105sks@gmail.com>
2026-07-20 13:12:14 +0530

    Time: 3 ms (45.51%), Space: 18.1 MB (66.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -4,30 +4,21 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        for (int x = 0; x < k; x++) {
+        vector<vector<int>> ans(m, vector<int>(n));
 
-            vector<vector<int>> a = grid;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % (m * n);
 
-                    if (j == n - 1) {
-                        if (i == m - 1) {
-                            a[0][0] = grid[i][j];
-                        }
-                        else {
-                            a[i + 1][0] = grid[i][j];
-                        }
-                    }
-                    else {
-                        a[i][j + 1] = grid[i][j];
-                    }
-                }
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans[newRow][newCol] = grid[i][j];
             }
-
-            grid = a; 
         }
 
-        return grid;
+        return ans;
     }
 };
