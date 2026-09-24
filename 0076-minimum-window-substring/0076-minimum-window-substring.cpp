9b1fa1c9985daab5de0a664a3540b598f0b977f7
Shivans Sinha <7105sks@gmail.com>
2026-09-24 16:16:48 +0530

    Time: 16 ms (29.03%), Space: 11.6 MB (43.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,50 +2,36 @@ class Solution {
 public:
     string minWindow(string s, string t) {
 
-        unordered_map<char,int> freq;
-        unordered_map<char,int> freq1;
-
-        int have = 0;
-
-        for(int i = 0; i < t.size(); i++){
+        unordered_map<char, int> freq;
+        unordered_map<char, int> freq1;
+        for (int i = 0; i < t.size(); i++) {
             freq[t[i]]++;
         }
-
-        int h = freq.size();
+        int longest = INT_MAX;
+        int start = 0;
+        int have = 0;
         int l = 0;
-
-        int bestStart = 0;
-        int bestLen = INT_MAX;
-
-        for(int r = 0; r < s.size(); r++){
-
-            freq1[s[r]]++;
-
-            if(freq.count(s[r]) && freq1[s[r]] == freq[s[r]]){
+        int h = freq.size();
+        for (int i = 0; i < s.size(); i++) {
+            freq1[s[i]]++;
+            if(freq.count(s[i]) && freq[s[i]] == freq1[s[i]]){
                 have++;
             }
-
-            while(have == h){
-
-                // Just remember indices, DON'T create substring
-                if(r - l + 1 < bestLen){
-                    bestLen = r - l + 1;
-                    bestStart = l;
+            while (have == h) {
+                if (longest > i - l + 1) {
+                    longest = i - l + 1;
+                    start = l;
                 }
-
                 freq1[s[l]]--;
-
-                if(freq.count(s[l]) && freq1[s[l]] < freq[s[l]]){
+                if (freq.count(s[l]) && freq1[s[l]] < freq[s[l]]) {
                     have--;
                 }
-
                 l++;
             }
         }
-
-        if(bestLen == INT_MAX)
+        if(longest == INT_MAX)
             return "";
 
-        return s.substr(bestStart, bestLen);
+        return s.substr(start, longest);
     }
 };
