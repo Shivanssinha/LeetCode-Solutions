1c6d5aa278fb2c42ff33eb4c08e7a3dee1385e96
Shivans Sinha <7105sks@gmail.com>
2026-09-26 14:29:00 +0530

    Time: 27 ms (43.12%), Space: 139.3 MB (49.48%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        vector<int> ans;
+        deque<int> dq;   // stores indices
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Remove elements that are outside the window
+            while (!dq.empty() && dq.front() <= i - k)
+                dq.pop_front();
+
+            // Remove smaller elements from the back
+            while (!dq.empty() && nums[dq.back()] <= nums[i])
+                dq.pop_back();
+
+            // Add current index
+            dq.push_back(i);
+
+            // Start adding answers when first window is complete
+            if (i >= k - 1)
+                ans.push_back(nums[dq.front()]);
+        }
+
+        return ans;
+    }
+};
